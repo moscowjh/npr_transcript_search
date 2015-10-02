6784630f652a7b9abec3b24350343782901f7187
Jason Horowitz <horowitz.jason@gmail.com>
2015-10-01 20:48:32 -0400

    Update npr_transcript_search.py

--- npr_transcript_search.py
+++ npr_transcript_search.py
@@ -13,7 +13,7 @@ key = "MDE5ODEzODc3MDE0MzYyOTk5NzlmNjM1NQ001"
 url = 'http://api.npr.org/query?apiKey='
 url += key
 url += "&numResults=5&format=JSON"
-search_string = raw_input("Enter your search query.")
+search_string = raw_input("Enter your search query:  ")
 url += "&searchterm=" + quote(search_string)
 #print url
 
@@ -26,3 +26,23 @@ for story in json_obj['list']['story']:
     print "DATE: " + story['storyDate']['$text']
     print "TEASER: " + story['teaser']['$text']
     print "NPRID:" + story['id'] + "\n"
+
+# Offer to return transcript of story if user inputs NPR ID of story
+# transcript API call URL
+url = 'http://api.npr.org/transcript?apiKey='
+url += key
+url += '&format=json&id='
+npr_id = raw_input("Enter the NPR ID to print transcript of a story:  ")
+url += npr_id
+
+response = urlopen(url)
+j = load(response)
+
+# print transcript paragraphs
+for paragraph in j["paragraph"]:
+	print paragraph["$text"] + "\n"
+
+# uncomment 3 lines below to see JSON output to file
+# f = open('output.json', 'w')
+# f.write(dumps(j, indent=4))
+# f.close()
